08f08f4b7f15f557ab399430be75ffdca41c0dc4
Meng <liokmkoil@gmail.com>
2016-07-28 00:33:13 +1200

    Enable postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 var path = require('path');
 var HtmlPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-  entry: path.join(APP_DIR, 'index'),
+  entry: [
+    'babel-polyfill',
+    path.join(APP_DIR, 'index'),
+  ]
   output: {
     path: path.resolve(__dirname, 'dist'),
     // Need a absolute path to work with history api
@@ -15,13 +19,28 @@ var config = {
         test: /\.jsx?/,
         include: APP_DIR,
         loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!postcss-loader'
+        })
       }
     ]
+  },
+  postcss: function(webpack) {
+    return [
+      require('postcss-import')({ addDependencyTo: webpack }),
+      require('postcss-cssnext')(),
+      require('postcss-reporter')(),
+    ];
   },
   plugins: [
     new HtmlPlugin({
       template: path.join(APP_DIR, 'index.html')
-    })
+    }),
+    new ExtractTextPlugin({ filename: 'styles.css', allChunks: true })
   ]
 };
 
